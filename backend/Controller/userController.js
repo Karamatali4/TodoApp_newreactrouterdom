0b3fe199a9041f7dcb11eed65feef22841140784
Karamatali4 <maqpoon4@gmail.com>
2025-01-24 19:15:51 +0500

    creting updateUser Controller and changes in this file

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,10 +1,16 @@
 const User = require("../Model/users");
 
-// Routes
+
+// creating users
 const createUser = async (req, res) => {
+  try {
     const user = new User(req.body);
     await user.save();
     res.send(user);
+  } catch (error) {
+    console.log(error);
+  }
+    
   }
   
   const getUser = async (req, res) => {
@@ -12,9 +18,31 @@ const createUser = async (req, res) => {
     res.send(users);
   }
   
+  // get single user
   const getSingleUser =  async (req, res) => {
     const user = await User.findById(req.params.id);
     res.send(user);
   }
 
-  module.exports = {createUser,getUser,getSingleUser};
+  // for update user
+  const updateUser = async(req,res) => {
+    try {
+      const {id} = req.params.id;
+      const {name,age,email}  = req.body;
+      const updateUser = await User.findByIdAndUpdate(
+        id,
+        name,age,email,
+        {
+          new:true
+        }
+      );
+      if(!updateUser){
+        return res.status(404).json({message:"user not found"});
+      }
+      res.json(updateUser);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Error updating contact' });
+    }
+  }
+  module.exports = {createUser,getUser,getSingleUser,updateUser};
