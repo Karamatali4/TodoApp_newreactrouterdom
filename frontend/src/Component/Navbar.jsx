b030a860a0b0e2ef4fabc862b2de619534da888e
Karamatali4 <maqpoon4@gmail.com>
2025-02-15 23:52:15 +0500

    changes in in navbar

--- frontend/src/Component/Navbar.jsx
+++ frontend/src/Component/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <nav className="w-[50rem] h-[3rem] bg-cyan-700 mx-auto rounded-md mb-5">
       <ul className="text-white font-bold flex  h-[100%] items-center gap-5 justify-center">
 
-        <li className="bg-cyan-600 rounded-lg px-3 py-1"><Link to="/">Home</Link></li>
-        <li className="bg-cyan-600 rounded-lg px-3 py-1"><Link to="/users">Users Detail</Link></li>
+        <li className="bg-cyan-600 rounded-lg px-3 py-1 hover:bg-cyan-500 duration-150"><Link to="/">Home</Link></li>
+        <li className="bg-cyan-600 rounded-lg px-3 py-1 hover:bg-cyan-500 duration-150"><Link to="/users">Users Detail</Link></li>
         
       </ul>
     </nav>
